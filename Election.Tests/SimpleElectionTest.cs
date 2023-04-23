57df9e7a003325696bc72231fa210f3d6c9ffab7
enzo <enzo.bonansea@gmail.com>
2023-04-23 13:18:52 -0300

    test: simple election winner is whose have more votes

--- Election.Tests/SimpleElectionTest.cs
+++ Election.Tests/SimpleElectionTest.cs
@@ -1,3 +1,4 @@
+using Election.Interfaces;
 using Election.Objects;
 
 namespace Election.Tests;
@@ -14,5 +15,29 @@ public class SimpleElectionTest
     public void ItMustHaveCandidates()
     {
         Assert.Equal(SimpleElection.ItMustHaveCandidatesErrorMessage, Assert.Throws<ArgumentException>(() => new SimpleElection(new List<SimpleBallot>(), null)).Message);
+    }
+
+    [Fact]
+    public void ItMustWinWhoseHaveMoreVotes()
+    {
+        var voterOne = new SimpleVoter(1, "Voter one");
+        var voterTwo = new SimpleVoter(2, "Voter two");
+        var voterThree = new SimpleVoter(3, "Voter three");
+
+        var candidateOne = new SimpleCandidate(1, "Candidate one");
+        var candidateTwo = new SimpleCandidate(2, "Candidate three");
+        
+        var ballots = new List<SimpleBallot>
+        {
+            new SimpleBallot(new SimpleVote(voterOne, candidateOne)),
+            new SimpleBallot(new SimpleVote(voterTwo, candidateOne)),
+            new SimpleBallot(new SimpleVote(voterThree, candidateTwo)),
+        };
+
+        var election = new SimpleElection(ballots, new List<ICandidate>() { candidateOne, candidateTwo });
+        
+        election.CountVotes();
+        
+        Assert.Equal(candidateOne, election.Winner);
     }
 }

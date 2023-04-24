844de4cca3bb1c2d4c499cb4772962b70382bf47
enzo <enzo.bonansea@gmail.com>
2023-04-24 15:41:38 -0300

    test(RankedChoiceElectionRound): on tie, loser is last

--- Election.Tests/RankedChoiceElectionRoundTest.cs
+++ Election.Tests/RankedChoiceElectionRoundTest.cs
@@ -109,4 +109,35 @@ public class RankedChoiceElectionRoundTest
         var round = new RankedChoiceElectionRound(ballots, candidates);
         Assert.Equal(candidateTwo, round.Loser);
     }
+
+    [Fact]
+    public void OnTieLoserIsLast()
+    {
+        var voterOne = new SimpleVoter(1, "Voter one");
+        var voterTwo = new SimpleVoter(2, "Voter two");
+        var voterThree = new SimpleVoter(3, "Voter three");
+        var candidateOne = new SimpleCandidate(1, "Candidate one");
+        var candidateTwo = new SimpleCandidate(2, "Candidate two");
+        var candidateThree = new SimpleCandidate(3, "Candidate three");
+        var candidates = new List<ICandidate> { candidateOne, candidateTwo, candidateThree };
+        // Candidate one: 2 first-preference votes (66.66%)
+        // Candidate two: 1 first-preference votes (33.33%)
+        var ballots = new List<RankedChoiceBallot>
+        {
+            new RankedChoiceBallot(new List<RankedChoiceVote>
+            {
+                new RankedChoiceVote(voterOne, candidateOne, 1),
+            }),
+            new RankedChoiceBallot(new List<RankedChoiceVote>
+            {
+                new RankedChoiceVote(voterTwo, candidateOne, 1),
+            }),
+            new RankedChoiceBallot(new List<RankedChoiceVote>
+            {
+                new RankedChoiceVote(voterThree, candidateTwo,  1),
+            }),
+        };
+        var round = new RankedChoiceElectionRound(ballots, candidates);
+        Assert.Equal(candidateThree, round.Loser);
+    }
 }

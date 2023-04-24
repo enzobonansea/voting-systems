401a75476eee23e9b65127da96a883a4778207bd
enzo <enzo.bonansea@gmail.com>
2023-04-24 15:35:30 -0300

    feat(RankedChoiceElectionRound): winner

--- Election/Objects/RankedChoiceElectionRound.cs
+++ Election/Objects/RankedChoiceElectionRound.cs
@@ -8,12 +8,22 @@ namespace Election.Objects
     public class RankedChoiceElectionRound
     {
         private Dictionary<ICandidate, int> firstPreferenceVotesPerCandidate;
+        private ICandidate winner;
 
         public RankedChoiceElectionRound(IEnumerable<RankedChoiceBallot> ballots, IEnumerable<ICandidate> candidates)
         {
             var firstPreferenceVotes = ballots.SelectMany(ballot => ballot.Votes).Where(vote => vote.Rank == 1);
             this.firstPreferenceVotesPerCandidate = candidates.ToDictionary(candidate => candidate, _ => 0);
             foreach (var vote in firstPreferenceVotes) this.firstPreferenceVotesPerCandidate[vote.Candidate] += 1;
+
+            this.winner = this.firstPreferenceVotesPerCandidate
+                .OrderByDescending(votesPerCandidate => votesPerCandidate.Value)
+                .First().Key;
+        }
+
+        public ICandidate Winner
+        {
+            get => this.winner;
         }
 
         public int GetFirstPreferenceVotes(ICandidate candidate)
